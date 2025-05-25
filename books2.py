5e6bc1658368a6fa5057b18d2c2bb298bdfeb6eb
MidnightJump <desmond597@outlook.com>
2025-05-25 20:54:04 +0800

    fix:test

--- books2.py
+++ books2.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI, Path, Query, HTTPException, Body
+from fastapi import Body, FastAPI, HTTPException, Path, Query
 from pydantic import BaseModel, Field
 from starlette import status
 
